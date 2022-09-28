e82c47923354d0060b7aeec3b8d8d7b27c9ed06b
한준호 <hanjunho@hanjunhoui-MacBookPro.local>
2022-09-28 23:30:30 +0900

    feat: react-query

--- src/util/svgFactory.ts
+++ src/util/svgFactory.ts
@@ -1,9 +1,13 @@
-export const svgFactory = (name, color) => {
+import { StaticImageData } from "next/image";
+
+export const svgFactory = (name: string, color: string) => {
   console.log("design system");
   console.log(name, color);
-  getIcon(name);
+  const Icon = getIcon(name);
+
+  // <Icon/>
 };
 
-const getIcon = (name: string) => {
+const getIcon = (name: string): StaticImageData => {
   return require(`@/public/assets/${name}.svg`);
 };
