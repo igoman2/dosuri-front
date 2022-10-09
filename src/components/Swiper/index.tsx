72dc4efccfd612ce0330af4de2e88c987abd5f72
igoman2 <igoman2@ajou.ac.kr>
2022-10-09 19:13:12 +0900

    feat: image swiper

--- src/components/Swiper/index.tsx
+++ src/components/Swiper/index.tsx
@@ -1,30 +1,45 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Scrollbar, A11y } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
+import SampleImage from "@/public/assets/sample.png";
+import Image from "next/image";
 
 const DoSwiper = () => {
   return (
     <Swiper
       style={{
-        width: "335px",
-        height: "170px",
-        backgroundColor: "#FFF5F1",
-        borderRadius: "12px",
+        width: "100%",
       }}
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={8}
-      initialSlide={1}
-      centeredSlides={true}
+      modules={[Scrollbar, A11y]}
+      scrollbar={{ draggable: true }}
+      spaceBetween={6}
+      initialSlide={0}
+      slidesPerView={2}
       pagination={{
         clickable: true,
       }}
       autoplay={{ delay: 3000 }}
     >
-      <SwiperSlide>슬라이더1</SwiperSlide>
-      <SwiperSlide>슬라이더2</SwiperSlide>
-      <SwiperSlide>슬라이더3</SwiperSlide>
+      <SwiperSlide>
+        <Image src={SampleImage} alt="hospitalImage" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={SampleImage} alt="hospitalImage" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={SampleImage} alt="hospitalImage" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={SampleImage} alt="hospitalImage" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={SampleImage} alt="hospitalImage" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={SampleImage} alt="hospitalImage" />
+      </SwiperSlide>
     </Swiper>
   );
 };
