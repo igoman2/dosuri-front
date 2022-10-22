fc01f9687fd91091f202071b1cb6ff01846d0081
igoman2 <igoman2@ajou.ac.kr>
2022-10-22 20:37:06 +0900

    feat: 내 포인트

---
+++ src/mock/pointHistory.ts
@@ -0,0 +1,18 @@
+export const pointHistory: PointHistory[] = [
+  {
+    date: "2022.12.31.",
+    content: "치료후기 인증",
+    point: "+1,000P",
+  },
+  {
+    date: "2022.09.31.",
+    content: "치료후기 인증",
+    point: "+1,000P",
+  },
+];
+
+interface PointHistory {
+  date: string;
+  content: string;
+  point: string;
+}
--- src/pages/mypage/point/index.tsx
+++ src/pages/mypage/point/index.tsx
@@ -1,7 +1,62 @@
+import Layout from "@/components/Layout";
+import HeaderDepth from "@/components/Layout/Header/HeaderDepth";
+import Divider from "@/components/UI/Divider";
+import { pointHistory } from "@/mock/pointHistory";
+import styled from "@emotion/styled";
 import React from "react";
 
 const Point = () => {
-  return <div></div>;
+  return (
+    <Layout header={<HeaderDepth />} footer={false}>
+      <PointWrapper>
+        <div className="sub-title">내 포인트 2,000P</div>
+        <ul className="list-section">
+          <Divider height={1} />
+
+          {pointHistory.map((history) => {
+            return (
+              <>
+                <li>
+                  <div>
+                    <div>{history.date}</div>
+                    <div>{history.content}</div>
+                  </div>
+                  <div>{history.point}</div>
+                </li>
+                <Divider height={1} />
+              </>
+            );
+          })}
+        </ul>
+      </PointWrapper>
+    </Layout>
+  );
 };
 
 export default Point;
+
+const PointWrapper = styled.div`
+  .sub-title {
+    color: ${(props) => props.theme.colors.purple};
+    font-size: ${(props) => props.theme.fontSizes.xl};
+    line-height: ${(props) => props.theme.lineHeights.xl};
+    font-weight: 700;
+    margin-bottom: 2.5rem;
+  }
+
+  .list-section {
+    font-size: ${(props) => props.theme.fontSizes.lg};
+    line-height: ${(props) => props.theme.lineHeights.lg};
+
+    li {
+      display: flex;
+      justify-content: space-between;
+      margin: 1rem 0;
+
+      div {
+        display: flex;
+        gap: 1rem;
+      }
+    }
+  }
+`;
