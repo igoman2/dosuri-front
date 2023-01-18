5ff67c2f196a3437469fac56f64dddc40e4f8955
igoman2 <igoman2@ajou.ac.kr>
2023-01-19 04:06:35 +0900

    fix: build error

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -43,7 +43,7 @@ const Header: FC<IHeaderProps> = ({ left, center, right }) => {
         {center && (
           <Link href="/search/input">
             <a>
-              <SearchBar />
+              <SearchBar inputText="" />
             </a>
           </Link>
         )}
