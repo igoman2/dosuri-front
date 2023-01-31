36251746afeae2e41028a20d5d6d178a574d921f
igoman2 <igoman2@ajou.ac.kr>
2023-02-01 01:09:17 +0900

    feat: 비용 입력칸 천단위 구분 기호 input type number로 설정

--- src/components/UI/emotion/Review/WriteReviewWrapper.tsx
+++ src/components/UI/emotion/Review/WriteReviewWrapper.tsx
@@ -40,6 +40,24 @@ export const WriteReviewWrapper = styled.div`
         color: ${(props) => props.theme.colors.grey};
       }
     }
+
+    &[comma-value] {
+      position: relative;
+      overflow: hidden;
+    }
+
+    &[comma-value]:before {
+      content: attr(comma-value);
+      font-size: ${(props) => props.theme.fontSizes.lg};
+      line-height: ${(props) => props.theme.lineHeights.lg};
+      position: absolute;
+      top: 1.2rem;
+      left: 0.8rem;
+    }
+
+    .treatment-price {
+      color: #fff;
+    }
   }
 
   .input-large {
