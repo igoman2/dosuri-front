61fcf467e4b3b3052e59b0325bcb00c0c787dfd6
igoman2 <igoman2@ajou.ac.kr>
2023-02-07 03:31:25 +0900

    feat: 구글 서치 콘솔

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -56,6 +56,10 @@ function MyApp({
           name="facebook-domain-verification"
           content="24ov5jif5wf1d1ci5yv25w6qb5eos1"
         />
+        <meta
+          name="google-site-verification"
+          content="nZSk2J4SaVkZwuucONktiqpBNgBIAxLqND64TrGGR0o"
+        />
       </Head>
 
       <DefaultSeo {...DEFAULT_SEO} />
