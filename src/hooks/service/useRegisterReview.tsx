29bcd5f3ad6cce51d3fa06c4e0bf0160ffa021a0
igoman2 <igoman2@ajou.ac.kr>
2023-03-16 02:27:34 +0900

    fix: 후기 등록 후 리스트 refetch

--- src/hooks/service/useRegisterReview.tsx
+++ src/hooks/service/useRegisterReview.tsx
@@ -1,12 +1,13 @@
 import { queryClient } from "@/service/react-query/queryClient";
 import { registerReview } from "@/service/apis/community";
 import { useMutation } from "react-query";
+import { queryKeys } from "@/service/react-query/constants";
 
 export function useRegisterReview() {
   const { mutate } = useMutation(registerReview, {
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["getCommunityListKeyword"],
+        queryKey: [queryKeys.community, "getCommunityListKeyword"],
         refetchInactive: true,
       });
     },
