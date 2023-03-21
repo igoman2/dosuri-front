53f9f881aebabc48c8d409937ca3fb9ae39c452e
igoman2 <igoman2@ajou.ac.kr>
2023-03-22 00:08:19 +0900

    fix: 아이폰 뒤로가기 스크롤 유지 config 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { withSentryConfig } = require("@sentry/nextjs");
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  experimental: {
+    scrollRestoration: true,
+  },
   reactStrictMode: false,
   swcMinify: true,
   compiler: {
