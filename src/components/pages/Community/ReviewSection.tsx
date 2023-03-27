810df1207cfd4d4e17fe4ed7fa4108c073b2e725
igoman2 <igoman2@ajou.ac.kr>
2023-03-28 02:46:37 +0900

    feat: 무한스크롤 뒤로가기 route as modal로 구현

--- src/components/pages/Community/ReviewSection.tsx
+++ src/components/pages/Community/ReviewSection.tsx
@@ -1,18 +1,18 @@
 import PostCard from "@/components/Card/PostCard";
 import PostBottom from "@/components/Card/PostCard/PostBottom";
-import useScrollRestoration from "@/hooks/useScrollRestoration";
 import api from "@/service/axiosConfig";
 import { queryKeys } from "@/service/react-query/constants";
 import { IHotCommunityResponse } from "@/service/types";
-import { useRouter } from "next/router";
-import React, { FC, useMemo } from "react";
+import React, { FC, useEffect, useMemo } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 import { useInfiniteQuery } from "react-query";
-import Image from "next/image";
-import ReviewBanner from "@/public/assets/review-banner.png";
-import { useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import _ from "lodash";
+import ModalPostCard from "@/components/Card/PostCard/ModalPostCard";
+import Image from "next/image";
+import ReviewBanner from "@/public/assets/review-banner.png";
 
 type Tab = {
   title: "전체" | "치료후기만 보기" | "질문/상담만 보기";
@@ -25,8 +25,6 @@ interface IReviewSectionProps {
 
 const ReviewSection: FC<IReviewSectionProps> = ({ currentTab }) => {
   const router = useRouter();
-  const theme = useTheme();
-  useScrollRestoration();
 
   const initialUrl = useMemo(() => {
     return `/community/v1/community/articles?article_type=${currentTab.value}&ordering=-created_at`;
@@ -52,18 +50,17 @@ const ReviewSection: FC<IReviewSectionProps> = ({ currentTab }) => {
     }
   );
 
-  const postClickHandler = (id: string) => {
-    router.push({
-      pathname: `/community/${id}`,
-    });
-  };
-
   const fetchNextList = () => {
     if (isFetching) {
       return;
     }
     fetchNextPage();
   };
+
+  const detailId = useMemo(() => {
+    return router.query.id;
+  }, [router]);
+  const showDetailModal = !_.isNil(detailId);
 
   /**
    *
@@ -88,46 +85,60 @@ const ReviewSection: FC<IReviewSectionProps> = ({ currentTab }) => {
     }
   };
 
+  useEffect(() => {
+    window.onpopstate = () => {
+      router.push(`/community`, `/community`, {
+        shallow: true,
+        scroll: false,
+      });
+    };
+  }, []);
+
   return (
-    <InfiniteScroll loadMore={fetchNextList} hasMore={hasNextPage}>
-      {communityList?.pages.map((pageData, index1) => {
-        return pageData.results.map((talk, index2) => {
-          return (
-            <>
-              {isBannerVisible(index1, index2) && (
-                <Link href="https://jade-grill-d5b.notion.site/4e50154c10c841b5a1eb9a8aac1355aa">
-                  <a target="_blank" rel="noopener noreferrer">
-                    <BannerWrapper>
-                      <span className="image">
-                        <Image
-                          src={ReviewBanner}
-                          width={250}
-                          height={60}
-                          alt="review-banner"
-                        />
-                      </span>
-                    </BannerWrapper>
+    <>
+      <InfiniteScroll loadMore={fetchNextList} hasMore={hasNextPage}>
+        {communityList?.pages.map((pageData, index1) => {
+          return pageData.results.map((talk, index2) => {
+            return (
+              <>
+                {isBannerVisible(index1, index2) && (
+                  <Link href="https://jade-grill-d5b.notion.site/4e50154c10c841b5a1eb9a8aac1355aa">
+                    <a target="_blank" rel="noopener noreferrer">
+                      <BannerWrapper>
+                        <span className="image">
+                          <Image
+                            src={ReviewBanner}
+                            width={250}
+                            height={60}
+                            alt="review-banner"
+                          />
+                        </span>
+                      </BannerWrapper>
+                    </a>
+                  </Link>
+                )}
+                <Link
+                  href={`/community?id=${talk.uuid}`}
+                  as={`/community/${talk.uuid}`}
+                  scroll={false}
+                  shallow={true}
+                  key={talk.uuid}
+                >
+                  <a>
+                    <PostCard
+                      review={talk}
+                      hasBackground={true}
+                      bottom={<PostBottom review={talk} type="list" />}
+                    />
                   </a>
                 </Link>
-              )}
-              <div
-                css={{
-                  cursor: "pointer",
-                }}
-                onClick={() => postClickHandler(talk.uuid)}
-                key={talk.uuid}
-              >
-                <PostCard
-                  review={talk}
-                  hasBackground={true}
-                  bottom={<PostBottom review={talk} type="list" />}
-                />
-              </div>
-            </>
-          );
-        });
-      })}
-    </InfiniteScroll>
+              </>
+            );
+          });
+        })}
+      </InfiniteScroll>
+      {showDetailModal && <ModalPostCard postId={detailId as string} />}
+    </>
   );
 };
 
