1a5171a7258ca52d1139108d128181d751b4e90a
igoman2 <igoman2@ajou.ac.kr>
2023-04-02 19:30:18 +0900

    fix: 메타 태그 위치 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -61,16 +61,101 @@ class MyDocument extends Document {
             content="https://dosuri-image.dosuri.site/common/favicon.png"
           />
 
-          <meta property="og:title" content="도수리" />
-          <meta
-            property="og:description"
-            content="도수 통증치료 병원정보는 도수리"
+          <link
+            rel="icon"
+            href="https://dosuri-image.dosuri.site/common/favicon-16x16.png"
           />
-          <meta property="og:url" content="https://www.dosuri.site" />
-          <meta
-            property="og:image"
-            content="https://dosuri-image.dosuri.site/common/1200x600.png"
+          <link
+            rel="shortcut icon"
+            href="https://dosuri-image.dosuri.site/common/favicon-16x16.png"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="https://dosuri-image.dosuri.site/common/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="https://dosuri-image.dosuri.site/common/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="https://dosuri-image.dosuri.site/common/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="https://dosuri-image.dosuri.site/common/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="https://dosuri-image.dosuri.site/common/favicon-16x16.png"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-TileImage"
+            content="https://dosuri-image.dosuri.site/common/ms-icon-144x144.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <link rel="canonical" href="https://www.dosuri.site" />
+
+          <meta property="product:brand" content="도수리" />
+          <meta property="product:availability" content="in stock" />
+          <meta property="product:condition" content="new" />
+          <meta property="product:price:amount" content="10000" />
+          <meta property="product:price:currency" content="KRW" />
         </Head>
         <body>
           <Main />
