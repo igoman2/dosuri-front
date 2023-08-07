3bbc52d75780c383c0311f6187da7845bf4f34d9
kitkat-mk <red.jade@icloud.com>
2023-08-07 15:58:49 +0900

    fix: 모바일/웹브라우저 구분해서 모달/배너 띄우기

---
+++ src/constants/Application.ts
@@ -0,0 +1,5 @@
+export const PLAY_STORE =
+  "https://play.google.com/store/apps/details?id=com.ytw418.dosuriapp";
+export const APP_STORE =
+  "https://apps.apple.com/kr/app/%EB%8F%84%EC%88%98%EB%A6%AC/id6448676216";
+export const INSTALL_APP_EXP = 1;
