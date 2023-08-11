d3929e889058b82693094fd690d270ef5ec9068a
igoman2 <igoman2@ajou.ac.kr>
2023-08-11 17:30:40 +0900

    feat: 현재 좌표를 쿠키에서 캐싱한다

--- src/hooks/useGeolocation.tsx
+++ src/hooks/useGeolocation.tsx
@@ -1,6 +1,8 @@
 import { DefaultLocation } from "@/constants/Map";
 import { Location } from "@/types/location";
 import { useEffect, useState } from "react";
+import { getCookie, setCookie } from "cookies-next";
+import dayjs from "dayjs";
 
 interface locationType {
   loaded: boolean;
@@ -18,6 +20,17 @@ const useGeolocation = () => {
   const onSuccess = (location: {
     coords: { latitude: number; longitude: number };
   }) => {
+    // 한 번 가져온 위치는 5분동안 캐싱
+    setCookie(
+      "getLocation",
+      {
+        latitude: location.coords.latitude.toString(),
+        longitude: location.coords.longitude.toString(),
+      },
+      {
+        expires: dayjs().add(5, "minutes").toDate(),
+      }
+    );
     setLocation({
       loaded: true,
       coordinates: {
@@ -40,17 +53,30 @@ const useGeolocation = () => {
   };
 
   useEffect(() => {
-    // navigator 객체 안에 geolocation이 없다면
-    // 위치 정보가 없는 것.
-    if (!("geolocation" in navigator)) {
-      onError({
-        code: 0,
-        message: "Geolocation not supported",
+    const cachedLocation = JSON.parse(getCookie("getLocation") as string);
+
+    // 쿠키에 위치 정보가 있다면
+    if (cachedLocation) {
+      setLocation({
+        loaded: true,
+        coordinates: {
+          latitude: Number(cachedLocation.latitude),
+          longitude: Number(cachedLocation.longitude),
+        },
       });
     } else {
-      navigator.geolocation.getCurrentPosition(onSuccess, onError, {
-        enableHighAccuracy: true,
-      });
+      // navigator 객체 안에 geolocation이 없다면
+      // 위치 정보가 없는 것.
+      if (!("geolocation" in navigator)) {
+        onError({
+          code: 0,
+          message: "Geolocation not supported",
+        });
+      } else {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+          enableHighAccuracy: true,
+        });
+      }
     }
   }, []);
 
