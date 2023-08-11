765db5422f90f2e6c2f8badcbe4b4850a8110d93
igoman2 <igoman2@ajou.ac.kr>
2023-08-11 14:41:04 +0900

    feat: 의사 상세 화면에 병원 상담 예약 버튼 추가

--- src/pages/hospital/[id]/doctor/[doctorId].tsx
+++ src/pages/hospital/[id]/doctor/[doctorId].tsx
@@ -1,18 +1,55 @@
+import Button from "@/components/Button";
 import DoctorCard from "@/components/Card/DoctorCard";
 import Layout from "@/components/Layout";
 import HeaderDepth from "@/components/Layout/Header/HeaderDepth";
+import { modalState, modalContentState } from "@/components/Modal/store";
 import { useGetDoctorInfo } from "@/hooks/service/useGetDoctorInfo";
+import { createReservation } from "@/service/apis/hospital";
+import theme from "@/styles/theme";
 import { useTheme } from "@emotion/react";
+import styled from "@emotion/styled";
 import { NextPageContext } from "next";
+import router from "next/router";
 import React from "react";
+import { useSetRecoilState } from "recoil";
 
 interface IDoctorInformationProps {
+  id: string;
   doctorId: string;
 }
 
-const DoctorInformation = ({ doctorId }: IDoctorInformationProps) => {
+const DoctorInformation = ({ id, doctorId }: IDoctorInformationProps) => {
   const { doctorInfo } = useGetDoctorInfo({ doctorId });
   const theme = useTheme();
+  const setNoticeModal = useSetRecoilState(modalState);
+  const setNoticeModalContent = useSetRecoilState(modalContentState);
+  const handleReservationClick = async () => {
+    try {
+      await createReservation({ hospital: id });
+      setNoticeModal({ isActive: true });
+      setNoticeModalContent({
+        title: "",
+        content: "예약이 신청되었습니다. 병원에서 곧 전화를 드립니다.",
+        actionCancel: {
+          text: "",
+          action: () => {},
+        },
+        actionWarn: {
+          text: "",
+          action: () => {},
+        },
+        actionConfirm: {
+          text: "확인",
+          action: () => {
+            setNoticeModal({ isActive: false });
+            router.back();
+          },
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <Layout header={<HeaderDepth />} footer={false}>
       <DoctorCard doctor={doctorInfo} isLink={false} />
@@ -80,6 +117,17 @@ const DoctorInformation = ({ doctorId }: IDoctorInformationProps) => {
           </div>
         ))}
       </div>
+      <SaleButtonWrapper>
+        <Button
+          text="도수치료 상담 신청하기"
+          width="100%"
+          height="5.2rem"
+          borderRadius="0.3rem"
+          backgroundColor={theme.colors.purple_light}
+          bold
+          onClick={handleReservationClick}
+        ></Button>
+      </SaleButtonWrapper>
     </Layout>
   );
 };
@@ -88,11 +136,26 @@ export default DoctorInformation;
 
 export const getServerSideProps = async (context: NextPageContext) => {
   const { query } = context;
-  const { doctorId, tab } = query;
+  const { id, doctorId, tab } = query;
   return {
     props: {
+      id,
       doctorId,
       tab: tab ?? "price",
     },
   };
 };
+
+const SaleButtonWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  margin: 0 2rem;
+  width: calc(100% - 4rem);
+  max-width: 40rem;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  padding: 1rem 0;
+  background-color: ${(props) => props.theme.colors.white};
+  z-index: 50;
+`;
