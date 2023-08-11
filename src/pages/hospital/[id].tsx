b9ac170add5cc68cf9e7a1f0473c42041e476c31
igoman2 <igoman2@ajou.ac.kr>
2023-08-11 14:05:35 +0900

    feat: 도수치료 상담 신청 버튼 클릭시 동의를 받지 않고 바로 신청하도록 절차를 줄입니다

--- src/pages/hospital/[id].tsx
+++ src/pages/hospital/[id].tsx
@@ -1,13 +1,11 @@
 import React, { FC, Suspense, useEffect, useState } from "react";
 import {
+  createReservation,
   getHospitalInfo,
   getHospitalTreatments,
   toggleHospitalThumbup,
 } from "@/service/apis/hospital";
-import Image from "next/image";
-import Checkbox from "@/components/Checkbox/Checkbox";
 import { useMutation, useQuery } from "react-query";
-import { BottomSheet } from "react-spring-bottom-sheet";
 import Button from "@/components/Button";
 import DoSwiper from "@/components/DoSwiper";
 import Doctors from "@/components/domain/Hospital/Doctors";
@@ -15,7 +13,6 @@ import HeaderDepth from "@/components/Layout/Header/HeaderDepth";
 import Icon from "@/util/Icon";
 import Information from "@/components/domain/Hospital/Information";
 import Layout from "@/components/Layout";
-import Link from "next/link";
 import { NextPageContext } from "next";
 import { NextSeo } from "next-seo";
 import Price from "@/components/domain/Hospital/Price";
@@ -29,15 +26,10 @@ import theme from "@/styles/theme";
 import { useRouter } from "next/router";
 import ImageTextView from "@/components/CustomImage/ImageTextView";
 import useAuth from "@/hooks/useAuth";
-import formIcon from "@/public/assets/form_icon.png";
-import { FormikProvider, useFormik } from "formik";
-import { useRecoilState } from "recoil";
-import { reservationModalState } from "@/components/domain/Hospital/store";
-import ReservationModal from "@/components/domain/Hospital/ReservationModal";
-import { changePersonalInfoConsent, getUser } from "@/service/apis/user";
-import { userInfoState } from "@/store/user";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { HospitalInfoTabList } from "@/constants/Tab";
 import { TabItem } from "@/types/community";
+import { modalState, modalContentState } from "@/components/Modal/store";
 
 interface IHospitalInformationProps {
   id: string;
@@ -53,47 +45,8 @@ const HospitalInformation: FC<IHospitalInformationProps> = ({ id, tab }) => {
   const [isUp, setIsUp] = useState<boolean>();
   const router = useRouter();
   const { isLoggedIn } = useAuth();
-  const [open, setOpen] = useState(false);
-  const [modal, setModal] = useRecoilState(reservationModalState);
-  const [userInfo, setUserInfo] = useRecoilState(userInfoState);
-  const [consents, setConsents] = useState([
-    {
-      checked: false,
-      text: "전체 동의",
-    },
-    {
-      checked: false,
-      text: "개인정보 수집이용 동의 (필수)",
-    },
-    {
-      checked: false,
-      text: "개인정보 제3자 제공 동의 (필수)",
-    },
-    {
-      checked: false,
-      text: "민감정보 수집 이용 동의 (필수)",
-    },
-    {
-      checked: false,
-      text: "마케팅 목적 개인정보 수집이용 동의 (선택)",
-    },
-    {
-      checked: false,
-      text: "마케팅 정보 수신 (선택)",
-    },
-    {
-      checked: false,
-      text: "앱 Push",
-    },
-    {
-      checked: false,
-      text: "이메일",
-    },
-    {
-      checked: false,
-      text: "SMS",
-    },
-  ]);
+  const setNoticeModal = useSetRecoilState(modalState);
+  const setNoticeModalContent = useSetRecoilState(modalContentState);
 
   useEffect(() => {
     router.replace({
@@ -101,10 +54,6 @@ const HospitalInformation: FC<IHospitalInformationProps> = ({ id, tab }) => {
       query: { tab: currentTab.value },
     });
   }, [currentTab]);
-
-  function onDismiss() {
-    setOpen(false);
-  }
 
   const checkTabIsProtected = (tab: TabItem) => {
     return PROTECTED_TABS.includes(tab.value);
@@ -134,15 +83,6 @@ const HospitalInformation: FC<IHospitalInformationProps> = ({ id, tab }) => {
   });
 
   const uuid = hospitalInfoData?.uuid;
-
-  const formik = useFormik({
-    initialValues: {
-      consents: [],
-    },
-    onSubmit: () => {
-      setModal(true);
-    },
-  });
 
   const { data: hospitalTreatmentsData } = useQuery({
     queryKey: ["hospital-treatments", uuid],
@@ -188,59 +128,32 @@ const HospitalInformation: FC<IHospitalInformationProps> = ({ id, tab }) => {
     (image) => image?.signed_path
   );
 
-  const handleAgreeAll = () => {
-    setConsents((prev) => {
-      const tmp = [...prev];
-      const isAgreeAll = prev[0].checked;
-      tmp.forEach((consent) => (consent.checked = !isAgreeAll));
-      return tmp;
-    });
-  };
-
-  const setAgreeAll = (consents: any) => {
-    let agreeAll = true;
-    for (let i = 1; i < 6; i++) {
-      agreeAll = agreeAll && consents[i].checked;
-    }
-
-    return agreeAll;
-  };
-
-  const consentHandler = async () => {
-    const personalInfoConsentParams = {
-      agree_marketing_personal_info: consents[4].checked,
-      agree_general_push: true,
-      agree_marketing_push: consents[5].checked,
-      agree_marketing_email: consents[7].checked,
-      agree_marketing_sms: consents[8].checked,
-      uuid: userInfo.uuid,
-    };
-
+  const handleReservationClick = async () => {
     try {
-      const response = await changePersonalInfoConsent(
-        personalInfoConsentParams
-      );
-      const resp = await getUser();
-      const user = resp!;
-      setUserInfo((prev) => {
-        return {
-          ...prev,
-          setting: user.setting,
-        };
+      await createReservation({ hospital: id });
+      setNoticeModal({ isActive: true });
+      setNoticeModalContent({
+        title: "",
+        content: "예약이 신청되었습니다. 병원에서 곧 전화를 드립니다.",
+        actionCancel: {
+          text: "",
+          action: () => {},
+        },
+        actionWarn: {
+          text: "",
+          action: () => {},
+        },
+        actionConfirm: {
+          text: "확인",
+          action: () => {
+            setNoticeModal({ isActive: false });
+            router.back();
+          },
+        },
       });
-      setOpen(false);
-      formik.handleSubmit();
     } catch (e) {
       console.log(e);
     }
-  };
-
-  const canSubmit = () => {
-    return consents[1].checked && consents[2].checked && consents[3].checked;
-  };
-
-  const handleReservationClick = () => {
-    setOpen(true);
   };
 
   return (
@@ -313,10 +226,10 @@ const HospitalInformation: FC<IHospitalInformationProps> = ({ id, tab }) => {
               )}
             </Suspense>
           </div>
-          <div className={open || modal ? "disable" : ""}>
+          <div>
             <SaleButtonWrapper>
               <Button
-                text="도수리에서 예약하고 3,000원 적립받기"
+                text="도수치료 상담 신청하기"
                 width="100%"
                 height="5.2rem"
                 borderRadius="0.3rem"
@@ -328,255 +241,11 @@ const HospitalInformation: FC<IHospitalInformationProps> = ({ id, tab }) => {
           </div>
         </Hospital>
       </Layout>
-      {open && (
-        <BottomSheet
-          open={open}
-          onDismiss={onDismiss}
-          // snapPoints={({ minHeight }) => [minHeight + 65]}
-        >
-          <DosuriReservation>
-            <div className="form-title">
-              <div>예약을 원하시면 동의가 필요해요</div>
-              <Image
-                src={formIcon}
-                alt="formIcon"
-                width={41}
-                height={41}
-              ></Image>
-            </div>
-            <FormikProvider value={formik}>
-              <form>
-                <CheckboxWrapper>
-                  <div className="agree-all">
-                    <Checkbox
-                      text={consents[0].text}
-                      value={consents[0].checked}
-                      onClick={handleAgreeAll}
-                    />
-                  </div>
-                  <div className="agree">
-                    <Checkbox
-                      text={consents[1].text}
-                      value={consents[1].checked}
-                      onClick={() => {
-                        setConsents((prev) => {
-                          const tmp = [...prev];
-                          tmp[1].checked = !prev[1].checked;
-                          tmp[0].checked = setAgreeAll(tmp);
-                          return tmp;
-                        });
-                      }}
-                    />
-                    <Link href="https://dosuri.notion.site/a424b489456a4ee9af5f6067e2cc6718">
-                      <a target="_blank" rel="noopener noreferrer">
-                        자세히
-                      </a>
-                    </Link>
-                  </div>
-                  <div className="agree">
-                    <Checkbox
-                      text={consents[2].text}
-                      value={consents[2].checked}
-                      onClick={() => {
-                        setConsents((prev) => {
-                          const tmp = [...prev];
-                          tmp[2].checked = !prev[2].checked;
-                          tmp[0].checked = setAgreeAll(tmp);
-                          return tmp;
-                        });
-                      }}
-                    />
-                    <Link href="https://dosuri.notion.site/3-e502521051ea4b8481e0c0bfd0f67f6b">
-                      <a target="_blank" rel="noopener noreferrer">
-                        자세히
-                      </a>
-                    </Link>
-                  </div>
-                  <div className="agree">
-                    <Checkbox
-                      text={consents[3].text}
-                      value={consents[3].checked}
-                      onClick={() => {
-                        setConsents((prev) => {
-                          const tmp = [...prev];
-                          tmp[3].checked = !prev[3].checked;
-                          tmp[0].checked = setAgreeAll(tmp);
-                          return tmp;
-                        });
-                      }}
-                    />
-                    <Link href="https://dosuri.notion.site/591f0834abb54961a0fab1c24667a351">
-                      <a target="_blank" rel="noopener noreferrer">
-                        자세히
-                      </a>
-                    </Link>
-                  </div>
-                  <div className="agree">
-                    <Checkbox
-                      text={consents[4].text}
-                      value={consents[4].checked}
-                      onClick={() => {
-                        setConsents((prev) => {
-                          const tmp = [...prev];
-                          tmp[4].checked = !prev[4].checked;
-                          tmp[0].checked = setAgreeAll(tmp);
-                          return tmp;
-                        });
-                      }}
-                    />
-                    <Link href="https://dosuri.notion.site/8c696da9c8a94c45bf9893ca213a8d2e">
-                      <a target="_blank" rel="noopener noreferrer">
-                        자세히
-                      </a>
-                    </Link>
-                  </div>
-
-                  <Checkbox
-                    text={consents[5].text}
-                    value={consents[5].checked}
-                    onClick={() => {
-                      setConsents((prev) => {
-                        const tmp = [...prev];
-                        const agreeMarketing = prev[5].checked;
-                        tmp[5].checked = !agreeMarketing;
-                        tmp[6].checked = !agreeMarketing;
-                        tmp[7].checked = !agreeMarketing;
-                        tmp[8].checked = !agreeMarketing;
-                        tmp[0].checked = setAgreeAll(tmp);
-                        return tmp;
-                      });
-                    }}
-                  />
-                  <div className="agree-sub">
-                    <Checkbox
-                      text={consents[6].text}
-                      value={consents[6].checked}
-                      onClick={() => {
-                        setConsents((prev) => {
-                          const tmp = [...prev];
-                          tmp[6].checked = !prev[6].checked;
-                          if (
-                            !tmp[6].checked &&
-                            !prev[7].checked &&
-                            !prev[8].checked
-                          ) {
-                            tmp[5].checked = false;
-                          } else {
-                            if (!tmp[5].checked) tmp[5].checked = true;
-                          }
-                          tmp[0].checked = setAgreeAll(tmp);
-                          return tmp;
-                        });
-                      }}
-                    />
-                    <Checkbox
-                      text={consents[7].text}
-                      value={consents[7].checked}
-                      onClick={() => {
-                        setConsents((prev) => {
-                          const tmp = [...prev];
-                          tmp[7].checked = !prev[7].checked;
-                          if (
-                            !prev[6].checked &&
-                            !tmp[7].checked &&
-                            !prev[8].checked
-                          ) {
-                            tmp[5].checked = false;
-                          } else {
-                            if (!tmp[5].checked) tmp[5].checked = true;
-                          }
-                          tmp[0].checked = setAgreeAll(tmp);
-                          return tmp;
-                        });
-                      }}
-                    />
-                    <Checkbox
-                      text={consents[8].text}
-                      value={consents[8].checked}
-                      onClick={() => {
-                        setConsents((prev) => {
-                          const tmp = [...prev];
-                          tmp[8].checked = !prev[8].checked;
-                          if (
-                            !prev[6].checked &&
-                            !prev[7].checked &&
-                            !tmp[8].checked
-                          ) {
-                            tmp[5].checked = false;
-                          } else {
-                            if (!tmp[5].checked) tmp[5].checked = true;
-                          }
-                          tmp[0].checked = setAgreeAll(tmp);
-                          return tmp;
-                        });
-                      }}
-                    />
-                  </div>
-                </CheckboxWrapper>
-              </form>
-            </FormikProvider>
-            <Button
-              text="동의하고 신청하기"
-              width="100%"
-              height="5.2rem"
-              borderRadius="0.3rem"
-              backgroundColor={theme.colors.purple_light}
-              bold
-              onClick={consentHandler}
-              disabled={!canSubmit()}
-            />
-          </DosuriReservation>
-        </BottomSheet>
-      )}
-      <ReservationModal hospitalUuid={hospitalInfoData.uuid} />
     </>
   );
 };
 
 export default HospitalInformation;
-
-const DosuriReservation = styled.div`
-  margin: 1rem;
-  .form-title {
-    display: flex;
-    align-items: center;
-    gap: 2rem;
-    font-size: ${(props) => props.theme.fontSizes.lg};
-    line-height: ${(props) => props.theme.lineHeights.lg};
-    font-weight: bold;
-    margin-bottom: 2rem;
-  }
-`;
-
-const CheckboxWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  margin-bottom: 1rem;
-
-  .agree-all {
-    margin-bottom: 2rem;
-  }
-
-  .agree {
-    display: flex;
-    justify-content: space-between;
-    a {
-      align-items: center;
-      font-size: ${(props) => props.theme.fontSizes.md};
-      line-height: ${(props) => props.theme.lineHeights.md};
-      color: ${(props) => props.theme.colors.grey};
-      white-space: nowrap;
-    }
-  }
-
-  .agree-sub {
-    display: flex;
-    gap: 2.1rem;
-    margin-left: 4rem;
-    margin-bottom: 2rem;
-  }
-`;
 
 const Hospital = styled.div`
   position: absolute;
@@ -625,7 +294,7 @@ const SaleButtonWrapper = styled.div`
   right: 0;
   padding: 1rem 0;
   background-color: ${(props) => props.theme.colors.white};
-  z-index: 100;
+  z-index: 50;
 `;
 
 const SwiperWrapper = styled.div`
