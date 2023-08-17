93d0e6b0ab9eb603ce2a11301b85e85ddc076f08
igoman2 <igoman2@ajou.ac.kr>
2023-08-17 19:39:32 +0900

    test

--- src/util/isApple.ts
+++ src/util/isApple.ts
@@ -1,29 +1,28 @@
 import { isMobile } from "react-device-detect";
 
 const isApple = () => {
- const user = navigator.userAgent.toLowerCase();
-   if (!isMobile) {
-     if (
-       user.indexOf("apple") > -1 ||
-       user.indexOf("safari") > -1 ||
-       user.indexOf("mac") > -1
-     ) {
-       return true;
-     } else {
-       return false;
-     }
-   } else {
-     if (
-       user.indexOf("iphone") > -1 ||
-       user.indexOf("ipad") > -1 ||
-       user.indexOf("ipod") > -1 ||
-       user.indexOf("safari") > -1
-     ) {
-       return true;
-     } else {
-       return false;
-     }
-   }
+  const user = navigator.userAgent.toLowerCase();
+  if (!isMobile) {
+    if (
+      user.indexOf("apple") > -1 ||
+      user.indexOf("safari") > -1 ||
+      user.indexOf("mac") > -1
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (
+      user.indexOf("iphone") > -1 ||
+      user.indexOf("ipad") > -1 ||
+      user.indexOf("ipod") > -1
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
 export default isApple;
